9b3ab9794e7cf2b64a2445d8b08cc216c23dadf1
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-08-09 20:22:47 +0300

    add ul li

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello, samurai! Let's go!
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
