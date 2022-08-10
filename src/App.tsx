3c2dee3eb4a17851df3371466596f1bb229c342a
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-08-10 13:46:38 +0300

    add component Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,31 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            Hello, samurai! Let's go!
-
+        <div>
+            <Header/>
+            <div>
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
         </div>
     );
 }
+
+function Header() {
+    return (
+        <div>
+            <a href={'#'}>Home</a>
+            <a href={'#'}>News</a>
+            <a href={'#'}>Message</a>
+        </div>
+    )
+}
+
+
+
 
 export default App;
