e7a368d2ecb71d88f47bd62b13fa8f362dce81e1
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-08-19 14:43:39 +0300

    add component Profile and moved content

---
+++ src/comonents/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+
+            <div>
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="1"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Posts
+                </div>
+            </div>
+            <div>
+                <div>
+                    Post - 1
+                </div>
+                <div>post - 2</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
