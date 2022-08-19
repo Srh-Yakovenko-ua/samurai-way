4b9d8851bc3bcaf537c3435a5760be2dc030e02e
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-08-19 12:51:59 +0300

    delete component Header | Technologies

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
     return (
         <div>
-            <Header/>
-            <Technologies/>
+
         </div>
     );
 }
-
-function Header() {
-    return (
-        <div>
-            <a href={'#'}>Home</a>
-            <a href={'#'}>News</a>
-            <a href={'#'}>Message</a>
-        </div>
-    );
-}
-
-function Technologies() {
-    return (
-        <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-            <li>react</li>
-        </ul>
-    );
-}
-
 
 export default App;
+
+
+
+
+
+
