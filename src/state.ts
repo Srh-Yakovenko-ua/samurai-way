f934a04a2b2cf37ac7ee662e59a296e0b79238b0
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-09-02 15:49:21 +0300

    refactor

--- src/state.ts
+++ src/state.ts
@@ -1,40 +1,54 @@
 import React from 'react';
 
-
-
-export type DialogsType = {
-    id: number
-    name: string
+type MessagesType = {
+    id : number
+    message : string
+}
+type DialogsType = {
+    id : number
+    name : string
+}
+type DialogsPageType = {
+    dialogs : Array<DialogsType>
+    messages : Array<MessagesType>
+}
+type PostsType = {
+    id : number
+    message : string
+    likesCount : number
+}
+type ProfilePageType = {
+    posts : Array<PostsType>
+}
+type RootStateType = {
+    profilePage : ProfilePageType
+    dialogsPage : DialogsPageType
 }
 
-export const dialogs: Array<DialogsType> = [
-    {id: 1, name: 'Dimych'},
-    {id: 2, name: 'Andrey'},
-    {id: 3, name: 'Sveta'},
-    {id: 4, name: 'Sasha'},
-    {id: 5, name: 'Viktor'},
-    {id: 6, name: 'Valera'},
-];
+export let state : RootStateType = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi,How are you?', likesCount: 12},
+            {id: 2, message: 'It\'s my new post', likesCount: 11},
+        ];
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'},
+            {id: 5, name: 'Viktor'},
+            {id: 6, name: 'Valera'},
+        ],
+        messages: [
+            {id: 1, message: 'Hello'},
+            {id: 2, message: 'IT-LEARN'},
+            {id: 3, message: 'YO'},
+            {id: 4, message: 'YO'},
+            {id: 5, message: 'YO'},
+            {id: 6, message: 'YO'},
+        ];
+    },
 
-  export type MessageType = {
-    id: number
-    message: string
 }
-export const messages: Array<MessageType> = [
-    {id: 1, message: 'Hello'},
-    {id: 2, message: 'IT-LEARN'},
-    {id: 3, message: 'YO'},
-    {id: 4, message: 'YO'},
-    {id: 5, message: 'YO'},
-    {id: 6, message: 'YO'},
-];
-
-export type PostsType = {
-    id: number
-    message: string
-    likesCount: number
-}
-export const posts: Array<PostsType> = [
-    {id: 1, message: 'Hi,How are you?', likesCount: 12},
-    {id: 2, message: 'It\'s my new post', likesCount: 11},
-];
