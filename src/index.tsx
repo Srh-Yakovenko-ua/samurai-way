724f6742946abc811fa38ddd981a7f7f62cb5c00
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-09-02 16:04:24 +0300

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {state} from './state';
 
 
 
 
 
 ReactDOM.render(
-    <App/>,
+    <App RootState={state}/>,
     document.getElementById('root')
 );
