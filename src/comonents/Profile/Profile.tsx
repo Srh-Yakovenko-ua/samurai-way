bad11cfc42c3a4870ae0130eef296c58fa83273e
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-09-12 20:24:39 +0300

    change

--- src/comonents/Profile/Profile.tsx
+++ src/comonents/Profile/Profile.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MyPosts from './MyPosts/MyPosts';
 import {ProfileInfo} from './ProfileInfo/ProfileInfo';
-import {PostsType} from '../../state';
+import {changeNewText, PostsType} from '../../state';
 
 
 type PropsTypeProfile ={
     posts : Array<PostsType>
     addPost: (postMessage: string) => void
+    newPostText : string
+    changeNewText: (newText : string)=> void
 }
 
 export const Profile = (props : PropsTypeProfile) => {
@@ -15,7 +17,10 @@ export const Profile = (props : PropsTypeProfile) => {
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts posts={props.posts} addPost={props.addPost}/>
+            <MyPosts posts={props.posts}
+                     addPost={props.addPost}
+                     newPostText={props.newPostText}
+                     changeNewText={changeNewText}/>
         </div>
     );
 };
