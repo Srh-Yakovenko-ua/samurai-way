9a01346b7424342d0210b4c530d78a90a6923214
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-09-12 20:24:41 +0300

    change

--- src/renderTree.tsx
+++ src/renderTree.tsx
@@ -1,11 +1,14 @@
-import {addPost, RootStateType} from './state';
+import {addPost, changeNewText, RootStateType} from './state';
 import ReactDOM from 'react-dom';
 import App from './App';
 import React from 'react';
 
 export const renderTree = (state: RootStateType) => {
     ReactDOM.render(
-        <App RootState={state} addPost={addPost}/>,
+        <App RootState={state}
+             addPost={addPost}
+             newPostText={state.profilePage.newPostText}
+             changeNewText={changeNewText}/>,
         document.getElementById('root')
     );
 }
