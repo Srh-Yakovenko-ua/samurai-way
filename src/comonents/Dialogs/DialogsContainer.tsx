21e9441bc9c38c96fb0e5796d503d6cfc7c7b181
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-30 18:29:16 +0300

    removed props and reused through StoreContext

--- src/comonents/Dialogs/DialogsContainer.tsx
+++ src/comonents/Dialogs/DialogsContainer.tsx
@@ -2,25 +2,41 @@ import React, {ChangeEvent} from 'react';
 import {sendMessageCreator, updateNewMessageBodyCreator} from '../../Redux/dialogs-reducer';
 import {store, storeType} from '../../Redux/redux-store';
 import {Dialogs} from './Dialogs';
+import {StoreContext} from '../../StoreContext';
+import {ActionCreatorAddPost, ActionCreatorChangeText} from '../../Redux/profile-reducer';
+import MyPosts from '../Profile/MyPosts/MyPosts';
 
-type DialogsContainerType = { store: storeType }
+//type DialogsContainerType = { store: storeType }
 
-export const DialogsContainer: React.FC<DialogsContainerType> = (props) => {
-    const state = store.getState();
+export const DialogsContainer = () => {
 
-    const onSendMessageClick = () => {
-        const newMessageText = state.dialogsPage.newMessageText
-        store.dispatch(sendMessageCreator(newMessageText))
-    }
-    const onChangeMessageBody = (bodyText: string) => store.dispatch(updateNewMessageBodyCreator(bodyText))
 
 
     return (
-        <Dialogs dialogs={state.dialogsPage.dialogs}
-                 newMessageText={state.dialogsPage.newMessageText}
-                 messages={state.dialogsPage.messages}
-                 onSendMessageClick={onSendMessageClick}
-                 onChangeMessageBody={onChangeMessageBody}
-        />
-    );
-};
+        <StoreContext.Consumer>
+            {store => {
+                const onSendMessageClick = () => {
+                    const newMessageText = store.getState().dialogsPage.newMessageText
+                    store.dispatch(sendMessageCreator(newMessageText))
+                }
+                const onChangeMessageBody = (bodyText: string) => {
+                    store.dispatch(updateNewMessageBodyCreator(bodyText))
+                }
+
+                return (
+                    <Dialogs dialogs={store.getState().dialogsPage.dialogs}
+                             newMessageText={store.getState().dialogsPage.newMessageText}
+                             messages={store.getState().dialogsPage.messages}
+                             onSendMessageClick={onSendMessageClick}
+                             onChangeMessageBody={onChangeMessageBody}/>
+                )
+            }}
+
+
+        </StoreContext.Consumer>
+
+    )
+
+
+}
+
