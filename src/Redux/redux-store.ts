8b3898e3b5e536cd6e9f166c5728d93fe4dd049d
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-07 15:41:56 +0300

    add users

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -1,12 +1,14 @@
 import {combineReducers, createStore} from 'redux';
 import {profileReducers} from './profile-reducer';
 import {dialogsReducers} from './dialogs-reducer';
+import {usersReducer} from './users-reducer';
 
 export type storeType = typeof store
 
 let reducers = combineReducers({
     profilePage: profileReducers,
     dialogsPage: dialogsReducers,
+    usersPage : usersReducer,
 });
 
 
