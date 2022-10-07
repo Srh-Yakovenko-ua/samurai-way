9d626fcebe9347b2ba26bae6a776e995a8d30256
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-07 16:44:41 +0300

    add btn follow unfollow and set users

--- src/comonents/Users/Users.tsx
+++ src/comonents/Users/Users.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import { usersType} from '../../Redux/users-reducer';
+import {usersType} from '../../Redux/users-reducer';
 import styles from './user.module.css'
 
 type UsersPropsType = {
-    users : usersType[]
+    users: usersType[]
+    follow: (userID: number) => void
+    unfollow: (userID: number) => void
+    setUsers: (user: any) => void
 }
-export const Users : React.FC<UsersPropsType> = (props) => {
-    const {users} = props;
+export const Users: React.FC<UsersPropsType> = (props) => {
+    const {users, follow, unfollow, setUsers} = props;
+    if (users.length === 0) {
+        setUsers([
+            {
+                id: 1,
+                photoUrl: 'https://igate.com.ua/upload/photo/0001/0001/3383/6955/55.jpg',
+                followed: false,
+                fullName: 'Alexandr',
+                status: 'i am a boss',
+                location: {city: 'Florida', country: 'USA'}
+            },
+            {
+                id: 2,
+                photoUrl: 'https://igate.com.ua/upload/photo/0001/0001/3383/6955/55.jpg',
+                followed: false,
+                fullName: 'Andrew',
+                status: 'i am a boss',
+                location: {city: 'Kiev', country: 'Ukraine'}
+            },
+            {
+                id: 3,
+                photoUrl: 'https://igate.com.ua/upload/photo/0001/0001/3383/6955/55.jpg',
+                followed: false,
+                fullName: 'Dmitriy',
+                status: 'i am a boss too',
+                location: {city: 'Augsburg', country: 'Germany'}
+            },
+
+        ])
+    }
 
     return (
         <div>
@@ -16,7 +48,8 @@ export const Users : React.FC<UsersPropsType> = (props) => {
                         <img src={u.photoUrl} className={styles.userPhoto}/>
                     </div>
                     <div>
-                        <button>Follow</button>
+                        {u.followed ? <button onClick={() => unfollow(u.id)}>unFollow</button> :
+                            <button onClick={() => follow(u.id)}>Follow</button>}
                     </div>
                 </span>
                 <span>
