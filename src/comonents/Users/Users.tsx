efc90dc91d99b273066fa2b09162a1b524a91749
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-07 16:09:51 +0300

    created markup and styles

--- src/comonents/Users/Users.tsx
+++ src/comonents/Users/Users.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { usersType} from '../../Redux/users-reducer';
+import styles from './user.module.css'
 
-export const Users = () => {
+type UsersPropsType = {
+    users : usersType[]
+}
+export const Users : React.FC<UsersPropsType> = (props) => {
+    const {users} = props;
+
     return (
         <div>
-            USERS WILL BE HEAR
+            {users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                    </div>
+                    <div>
+                        <button>Follow</button>
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.fullName}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{u.location.country}</div>
+                        <div>{u.location.city}</div>
+                    </span>
+                </span>
+            </div>)}
         </div>
     );
 };
