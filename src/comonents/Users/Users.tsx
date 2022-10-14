51cb8c21fb92f9fa49c2083225380f03196cf9e1
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-14 15:14:53 +0300

    changed to class component

--- src/comonents/Users/Users.tsx
+++ src/comonents/Users/Users.tsx
@@ -10,23 +10,66 @@ type UsersPropsType = {
     unfollow: (userID: number) => void
     setUsers: (user: usersType[]) => void
 }
-export const Users: React.FC<UsersPropsType> = (props) => {
-        const {users, follow, unfollow, setUsers} = props;
+// export const Users: React.FC<UsersPropsType> = (props) => {
+//         const {users, follow, unfollow, setUsers} = props;
+//
+//         let getUsers = () => {
+//             if (users.length === 0) {
+//                 axios
+//                     .get('https://social-network.samuraijs.com/api/1.0/users')
+//                     .then(response => {
+//                         setUsers(response.data.items)
+//                     })
+//             }
+//         }
+//
+//
+//         return (
+//             <div>
+//                 <button onClick={getUsers}>Get Users</button>
+//                 {users.map(u => <div key={u.id}>
+//                 <span>
+//                     <div>
+//                         <img src={u.photos.small !== null ? u.photos.small : usersPhoto} className={styles.userPhoto}
+//                              alt={'avatar'}/>
+//                     </div>
+//                     <div>
+//                         {u.followed ? <button onClick={() => unfollow(u.id)}>unFollow</button> :
+//                             <button onClick={() => follow(u.id)}>Follow</button>}
+//                     </div>
+//                 </span>
+//                     <span>
+//                     <span>
+//                         <div>{u.name}</div>
+//                         <div>{u.status}</div>
+//                     </span>
+//                     <span>
+//                         <div>{'u.location.country'}</div>
+//                         <div>{'u.location.city'}</div>
+//                     </span>
+//                 </span>
+//                 </div>)}
+//             </div>
+//         );
+//     }
+// ;
+//
+export class Users extends React.Component<UsersPropsType> {
+    constructor(props: UsersPropsType) {
+        super(props);
+        const {setUsers} = this.props;
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                setUsers(response.data.items)
+            })
+    }
 
-        let getUsers = () => {
-            if (users.length === 0) {
-                axios
-                    .get('https://social-network.samuraijs.com/api/1.0/users')
-                    .then(response => {
-                        setUsers(response.data.items)
-                    })
-            }
-        }
 
-
+    render = () => {
+        const {users, follow, unfollow} = this.props;
         return (
             <div>
-                <button onClick={getUsers}>Get Users</button>
                 {users.map(u => <div key={u.id}>
                 <span>
                     <div>
@@ -52,5 +95,4 @@ export const Users: React.FC<UsersPropsType> = (props) => {
             </div>
         );
     }
-;
-
+}
