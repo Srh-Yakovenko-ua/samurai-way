aa3fbc0404104f89fc7b91a96cfe28556b554175
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-18 20:59:43 +0300

    create onPageChanged

--- src/comonents/Users/Users.tsx
+++ src/comonents/Users/Users.tsx
@@ -68,6 +68,15 @@ export class Users extends React.Component<UsersPropsType> {
                 this.props.setUsers(response.data.items)
             });
     }
+    onPageChanged = (pageNumber: number) => {
+        const {pageSize} = this.props;
+        this.props.setCurrentPage(pageNumber)
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
+    }
 
     render = () => {
         const {
@@ -77,13 +86,12 @@ export class Users extends React.Component<UsersPropsType> {
             pageSize,
             totalUsersCount,
             currentPage,
-            setCurrentPage
         } = this.props;
 
         const pagesCount = Math.ceil(totalUsersCount / pageSize);
         const pages = [];
         for (let i = 1; i <= pagesCount; i++) pages.push(i)
-        const pagesNumber = pages.map(p => <span onClick={() => setCurrentPage(p)}
+        const pagesNumber = pages.map(p => <span onClick={() => this.onPageChanged(p)}
                                                  className={currentPage === p ? styles.selectedPage : ''}>{p}</span>)
 
         return (
