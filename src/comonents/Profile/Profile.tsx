17b1ad9ef94f0fc2b4c1614f698ae5f7c01d756b
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-22 16:20:01 +0300

    added props and type

--- src/comonents/Profile/Profile.tsx
+++ src/comonents/Profile/Profile.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {ProfileInfo} from './ProfileInfo/ProfileInfo';
-import MyPostsContainer from './MyPosts/Post/MyPostsContainer';
+import {MyPostsContainer} from './MyPosts/Post/MyPostsContainer';
+import {ProfileType} from '../../Redux/profile-reducer';
 
 
-export const Profile = () => {
+type ProfileTPropsType = {
+    profile: ProfileType | null
+}
 
+export const  Profile: React.FC<ProfileTPropsType> = (props) => {
+    const {profile} = props
     return (
         <div>
-            <ProfileInfo/>
+            <ProfileInfo profile={profile}/>
             <MyPostsContainer/>
 
         </div>
--- src/comonents/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/comonents/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import style from './ProfileInfo.module.css'
+import {ProfileType} from '../../../Redux/profile-reducer';
+import {Preloader} from '../../common/Preloader/Preloader';
 
-export const ProfileInfo = () => {
+type ProfileInfoType = {
+    profile: ProfileType | null
+}
+
+export const ProfileInfo: React.FC<ProfileInfoType> = (props) => {
+    const {profile} = props
+    if(!profile){
+        return <Preloader/>
+    }
     return (
         <div>
             <div>
                 <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="1"/>
             </div>
             <div className={style.descriptionBlock}>
-                ava + description
+                <img src={profile.photos.large} alt=""/>
             </div>
         </div>
     );
