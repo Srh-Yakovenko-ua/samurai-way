edfa66c9a360ada3e938796e629128af48947664
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-28 22:08:23 +0300

    added auth

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -2,6 +2,7 @@ import {combineReducers, createStore} from 'redux';
 import {profileReducers} from './profile-reducer';
 import {dialogsReducers} from './dialogs-reducer';
 import {usersReducer} from './users-reducer';
+import {authReducer} from './auth-reducer';
 
 export type storeType = typeof store
 
@@ -9,8 +10,11 @@ let reducers = combineReducers({
     profilePage: profileReducers,
     dialogsPage: dialogsReducers,
     usersPage : usersReducer,
+    auth : authReducer,
 });
 
 
 export let store = createStore(reducers);
 export type RootReducerType = ReturnType<typeof reducers>;
+//@ts-ignore
+window.store = store
