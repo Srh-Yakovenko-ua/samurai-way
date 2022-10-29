818d91cb466a0a8d80153cbcb3ed3ff6f88db5bb
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-29 19:14:40 +0300

    props

--- src/comonents/Header/Header.tsx
+++ src/comonents/Header/Header.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import style from './Header.module.css'
+import {NavLink} from 'react-router-dom';
 
-export const Header = () => {
+type HeaderPropsType = {
+    isAuth: boolean
+    login: string | null
+}
+
+export const Header: React.FC<HeaderPropsType> = (props) => {
+    const {isAuth, login} = props;
+
+
     return (
         <header className={style.header}>
             <img
                 src="https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr"
                 alt="1"/>
+
+
+            <div className={style.loginBlock}>
+                {isAuth ? login : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
+
         </header>
     );
 };
