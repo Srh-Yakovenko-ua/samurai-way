2ded53c49ce901b00be95f1f07ce9bbe2ab3bde1
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-11-02 17:37:59 +0300

    added unFollowUser and followUser

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -4,10 +4,26 @@ import {instance} from './axios-instance';
 export const usersApi = {
     getUsers(currentPage: number = 1, pageSize: number = 5) {
         return instance
-            .get(`users?page=${currentPage}&count=${pageSize}`, {withCredentials: true})
+            .get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => response.data)
     },
-
+    unFollowUser(userID: number, unfollow: (userID: number) => void) {
+        instance
+            .delete(`follow/${userID}`,)
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    unfollow(userID)
+                }
+            })
+    },
+    followUser(userID: number, follow: (userID: number) => void) {
+        instance
+            .post(`/follow/${userID}`, {})
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    follow(userID)
+                }
+            })
+    }
 }
-
 
