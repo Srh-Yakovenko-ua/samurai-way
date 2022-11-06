557f5af50ca5b16e0c63674b9ecf76453f695e5d
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-11-06 19:50:17 +0300

    change component

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,25 @@ import React from 'react';
 import './App.css';
 import {Header} from './comonents/Header/Header';
 import {Navbar} from './comonents/Navbar/Navbar';
-import {Profile} from './comonents/Profile/Profile';
 import {Route} from 'react-router-dom';
 import {News} from './comonents/News/News';
 import {Music} from './comonents/Music/Music';
 import {Settings} from './comonents/Settings/Settings';
 import {DialogsContainer} from './comonents/Dialogs/DialogsContainer';
 import UsersContainer from './comonents/Users/UsersContainer';
-
+import ProfileContainer from './comonents/Profile/ProfileContainer';
+import HeaderContainer from './comonents/Header/HeaderContainer';
 
 
 const App = () => {
 
-
     return (
         <div className="app-wrapper">
-            <Header/>
+          <HeaderContainer/>
             <Navbar/>
             <div className="app-wrapper-content">
                 <Route path={'/dialogs'} component={DialogsContainer}/>
-                <Route path={'/profile'} component={Profile}/>
+                <Route path={'/profile/:userId?'} component={ProfileContainer}/>
                 <Route path={'/news'} component={News}/>
                 <Route path={'/music'} component={Music}/>
                 <Route path={'/users'} component={UsersContainer}/>
