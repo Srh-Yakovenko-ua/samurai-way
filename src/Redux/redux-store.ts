9d20b93fbec9c91d134f940c39c3e2330e86d3a7
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-12-04 21:44:59 +0300

    added thunk to store

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -1,20 +1,26 @@
-import {combineReducers, createStore} from 'redux';
+import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {profileReducers} from './profile-reducer';
 import {dialogsReducers} from './dialogs-reducer';
 import {usersReducer} from './users-reducer';
 import {authReducer} from './auth-reducer';
+import thunk from 'redux-thunk';
 
-export type storeType = typeof store
+//export type storeType = typeof store
 
 let reducers = combineReducers({
     profilePage: profileReducers,
     dialogsPage: dialogsReducers,
-    usersPage : usersReducer,
-    auth : authReducer,
+    usersPage: usersReducer,
+    auth: authReducer,
 });
 
 
-export let store = createStore(reducers);
+export const store = createStore(
+    reducers,
+    applyMiddleware(thunk)
+);
+
+
 export type RootReducerType = ReturnType<typeof reducers>;
 //@ts-ignore
 window.store = store
