479d8dc9c9d0b50d86d570469a8f922f320a21dc
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-12-08 22:10:19 +0300

    refactor to compose function  HOC DialogsContainer

--- src/comonents/Dialogs/DialogsContainer.tsx
+++ src/comonents/Dialogs/DialogsContainer.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {ComponentType} from 'react';
 import {DialogsPageType, sendMessageAC, updateNewMessageBodyAC} from '../../Redux/dialogs-reducer';
 import {Dialogs} from './Dialogs';
 import {connect} from 'react-redux';
 import {RootReducerType} from '../../Redux/redux-store';
 import {WithAuthRedirect} from '../../hoc/withAuthRedirect';
+import {compose} from 'redux';
 
 type mapStateToPropsType = {
     dialogsPage: DialogsPageType
@@ -16,7 +17,7 @@ type mapDispatchToPropsType = {
 }
 export type ownDialogsContainerType = mapStateToPropsType & mapDispatchToPropsType
 
-const mapStateToProps = (state: RootReducerType) : mapStateToPropsType => {
+const mapStateToProps = (state: RootReducerType): mapStateToPropsType => {
     return {
         dialogsPage: state.dialogsPage,
         newMessageText: state.dialogsPage.newMessageText,
@@ -24,14 +25,13 @@ const mapStateToProps = (state: RootReducerType) : mapStateToPropsType => {
     }
 }
 
-const AuthRedirectComponent = WithAuthRedirect(Dialogs)
-
-export const DialogsContainer = connect<mapStateToPropsType, mapDispatchToPropsType, {}, RootReducerType>(mapStateToProps, {
-    onSendMessageClick: sendMessageAC,
-    onChangeMessageBody: updateNewMessageBodyAC,
-})(AuthRedirectComponent);
-
-
+export const DialogsContainer = compose<ComponentType>(
+    connect<mapStateToPropsType, mapDispatchToPropsType, {}, RootReducerType>(mapStateToProps, {
+        onSendMessageClick: sendMessageAC,
+        onChangeMessageBody: updateNewMessageBodyAC,
+    }),
+    WithAuthRedirect,
+)(Dialogs)
 
 
 //  const DialogsContainer = () => {
