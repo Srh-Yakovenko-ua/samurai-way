e6be2d9e31286aae2f02d7c1ecfdba79e2ea6db1
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-12-11 22:27:05 +0300

    added a request to the server to get and change statusProfile

--- src/api/profileApi.ts
+++ src/api/profileApi.ts
@@ -7,5 +7,14 @@ export const profileApi = {
         return instance
             .get<ProfileType>(`profile/${userId}`)
             .then(response => response.data)
+    },
+    getProfileStatus(userId: string | undefined) {
+        return instance
+            .get(`profile/status/${userId}`)
+    },
+    updateProfileStatus(newStatusText: string) {
+        return instance
+            .put(`profile/status`, {status: newStatusText} )
     }
+
 }
